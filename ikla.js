023ecd32789e862d312fa214d60bb7db6a60aef3
oghiezr <oghiezr@gmail.com>
2017-10-30 00:31:46 +0700

    Create ikla.js

---
+++ ikla.js
@@ -0,0 +1,33 @@
+/* Javascript bot iklan di blogger
+by www.sembilanan.tk */
+
+var pop = document.createElement('script'); pop.type = 'text/javascript'; pop.setAttribute('data-cfasync', false);
+pop.src = "https://github.com/oghiezr/sembilanan/new/master";
+document.getElementsByTagName('head')[0].appendChild(pop);
+
+//jQuery Auto Click Chrome
+$(document).ready(function() { 
+$("div").trigger('click');
+});
+
+/*<![CDATA[/* */
+  var _pop = _pop || [];
+  _pop.push(['siteId', 1772994]);
+  _pop.push(['minBid', 0]);
+  _pop.push(['popundersPerIP', 0]);
+  _pop.push(['delayBetween', 0]);
+  _pop.push(['default', false]);
+  _pop.push(['defaultPerDay', 0]);
+  _pop.push(['topmostLayer', false]);
+  (function() {
+    var pa = document.createElement('script'); pa.type = 'text/javascript'; pa.async = true;
+    var s = document.getElementsByTagName('script')[0]; 
+    pa.src = '//c1.popads.net/pop.js';
+    pa.onerror = function() {
+      var sa = document.createElement('script'); sa.type = 'text/javascript'; sa.async = true;
+      sa.src = '//c2.popads.net/pop.js';
+      s.parentNode.insertBefore(sa, s);
+    };
+    s.parentNode.insertBefore(pa, s);
+  })();
+/*]]>/* */
